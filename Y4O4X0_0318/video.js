1be07e18b2c2817afe9028c26ef6479f7572450b
SZKrisztian <szaloczy6@gmail.com>
2024-03-20 13:36:23 +0100

    implement-js-functions

--- Y4O4X0_0318/video.js
+++ Y4O4X0_0318/video.js
@@ -24,6 +24,11 @@ function quite(){
     myVideo.volume = newVolume;
 }
 
+function loud(){
+   myVideo.volume = 1;
+}
+
+
 function slowRate(){
     var currentPlaybackRate = myVideo.playbackRate;
     var newPlaybackRate = currentPlaybackRate - 0.5;
@@ -44,4 +49,44 @@ function normalRate(){
    myVideo.playbackRate=1;
 }
 
-  
+function fasterRate(){
+    myVideo.playbackRate = 2;
+}
+
+function veryFastRate(){
+    myVideo.playbackRate = 5;
+}
+
+function small(){
+    myVideo
+}
+
+function makeSmall(){
+    myVideo.width = 300;
+}
+
+function normalSize(){
+    if(myVideo.width < 400 || myVideo.width > 500){
+        myVideo.width = 400;
+    }
+}
+
+function makeBig(){
+    myVideo.width = 600;
+}
+
+function isFinished(){
+    if(myVideo.ended){
+        window.alert("A video befejeződött");
+    } else {
+        window.alert("A videó még megy");
+    }
+}
+ 
+function getLength(){
+    window.alert(myVideo.duration.toFixed(2) + "mp");
+}
+
+function getCurrentPosition(){
+    window.alert("A videó a " + myVideo.currentTime.toFixed(2)+" másodpercnél jár");
+}
