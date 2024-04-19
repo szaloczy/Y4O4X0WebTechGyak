22ef096478fe7c5855f0bbbb695759445c4e188c
SZKrisztian <107873757+szaloczy@users.noreply.github.com>
2024-04-19 13:01:55 +0200

    finished

--- Y4O4X0_0415/2.js
+++ Y4O4X0_0415/2.js
@@ -1,36 +1,43 @@
-$(document).ready(() =>{
-  
-    //Lista két első elemét elrejti
-    $("#K1").click(function(){
-        $("ul").each(function(){
-            $(this).children("li:lt(2)").hide();
-        });
-        $("a").hide();
-    });
+function katIdeBtn() {
+    $("#katIdeBtn").each(function(){
+        $("p").hide();
+    })
+}
 
-    $("#K2").click(function(){
-        $("ul").each(function(){
-            $(this).children("li:lt(2)").hide();
-        });
-        $("a, #K2").hide();
+function elrejtK1() {
+    $("ul").each(function() {
+        $(this).children("li:lt(2)").hide();
     });
+    $("a").hide();
+}
 
-    $("#K3").click(function(){
-        $("h1, ul").each(function(){
-            $(this).hide();
-        });
-        $("a").hide();
-        $("header").hide();
+function elrejtK2() {
+    $("ul").each(function() {
+        $(this).children("li:lt(2)").hide();
     });
+    $("a, #K2").hide();
+}
 
-    // K4 gomb: elrejti a lista két első elemét, link szöveget és href-t
-    $("#K4").click(function(){
-        $("ul").each(function(){
-            $(this).children("li:lt(2)").hide();
-        });
-        $("a").each(function(){
-            $(this).text("");
-        });
-        $("a").hide();
+function elrejtK3(){
+    $("ul").each(function(){
+        $(this).children("li:lt(2)").hide();
     });
-})
+    $("header, a").hide();
+}
+
+function elrejtK4(){
+    $("ul").each(function(){
+        $(this).children("li:lt(2)").hide();
+    });
+    $("a, #linkszoveg").hide();
+}
+
+function elrejtK5(){
+    $("ul").each(function(){
+        $(this).children("li:lt(2)").hide();
+    });
+    $("a").hide();
+    $("table").each(function(){
+        $(this).find("tr:odd").hide();
+    });
+}
