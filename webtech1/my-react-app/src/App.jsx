f33c23797031c7a049d4c97b83eb6ee4fd22b482
Szaloczy <szaloczy6@gmail.com>
2024-09-18 20:29:29 +0200

    update-arrays-objects

--- webtech1/my-react-app/src/App.jsx
+++ webtech1/my-react-app/src/App.jsx
@@ -1,14 +1,7 @@
 import Footer from './Footer'
 import Header from './Header/Header'
-import Card from './Card'
-import Button from './Button/Button'
-import Student from './Student'
-import UserGreeting from './UserGreeting'
-import List from './List'
-import ProfilePicture from './ProfilePicture'
-import MyComponent from './MyComponent'
 import Counter from './Counter'
-import ColorPicker from './ColorPickerComponent'
+import MyComponent4 from './MyComponent4';
 
 
 function App() {
@@ -21,7 +14,7 @@ function App() {
   return(
     <>
     <Header></Header>
-    <Counter></Counter>
+    <MyComponent4></MyComponent4>
     <Footer></Footer>
     </>
   );
